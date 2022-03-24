65b334c9f542100651ce914518c9a1fa92750f5c
Grzegorz W <gerwazy911@gmail.com>
2022-03-24 18:17:39 +0100

    homeworkVeryHard

---
+++ task1.js
@@ -0,0 +1,113 @@
+
+const people = [
+    {
+      firstName: "Bartolomeo",
+      lastName: "Lozano",
+    },
+    {
+        firstName: "asdasdasd",
+        lastName: "Wjkasdkjas",
+      },
+  ];
+
+
+
+
+function createNickname(people){
+  const getNames
+}
+
+
+// const getThreeLastReverseLetters=arr.map(lastName=>{
+//     const reverse=lastName.reverse()
+//     const lettersArr=[]
+//     if(lastName.length>3){
+//         for(i=0;i<3;i++){
+//             let letter=getName[i]
+//             lettersArr.push(letter)  
+//         }
+//         return lettersArr.reverse()
+//     }
+//     else 
+//     for(i=0;i<getName.length;i++){
+//         let letter=getName[i]
+//         lettersArr.push(letter) 
+//      }
+
+//      return lettersArr.reverse()
+//     })
+//     for(i=0;i<3;i++){
+//         let letter=reverse[i]
+//         threeLetters.push(letter)
+//     }
+//  return threeLetters
+
+    
+       
+// // const grzegor=[" ",""]
+
+//        console.log (getThreeFirstReverseLetters)
+
+        // const getThreeLastReverseLetters=getLastNames.map(getLastNames=>{
+        //     const reverse=getLastNames.split("").reverse()
+        //     const threeLetters=[]
+        //     for(i=0;i<3;i++){
+        //         let letter=reverse[i]
+        //         threeLetters.push(letter)
+        //     }
+        //  return threeLetters})
+
+
+        // // // console.log(getThreeLastReverseLetters)
+        // // // console.log(getThreeFirstReverseLetters)
+        // // // const nicknames=(getThreeFirstReverseLetters,getThreeLastReverseLetters).map(firstLetters,LastLetters)=>{
+        // // //     const nickname=firstLetters.concat(lastLetters)
+        // // //     return nickname
+        // // // })
+        
+        // // // const firstLetters=getThreeFirstReverseLetters.map(letters=>letters)
+        // // console.log(nicknames)
+        // //     // for(i=0;i<getThreeFirstReverseLetters.length;i++)
+        // //     // nickname[i]=getThreeFirstReverseLetters.concat(getThreeLastReverseLetters)   
+        
+        
+        // // // const nickname=createNickname(getThreeFirstReverseLetters(getName),getThreeLastReverseLetters(getLastName))
+        // // // const changeToString=nickname=>nickname.join("")
+        // // // const nicknameChanged=changeToString(nickname)
+        // // // const nicknameToLowerCase=nicknameChanged=>nicknameChanged.toLowerCase() 
+        // // // const nicknameLowerCase=nicknameToLowerCase(nicknameChanged)  // return nicknameLowerCase and it's converted to string
+        // // // const nicknameToArr=nicknameLowerCase=>nicknameLowerCase.split("")
+        // // // const nicknameArr= nicknameToArr(nicknameLowerCase)
+        // // // const firstLetterUppercase=nicknameArr[0].toUpperCase()
+        // // // const firstLetter=[]
+        // // // firstLetter.push(firstLetterUppercase)
+        // // // const nicknameCutFirstIndex=nicknameArr=>nicknameArr.slice(1)
+        // // // const nicknameNoFirstIndex=nicknameCutFirstIndex(nicknameArr)
+        // // // const readyNickname=(firstLetter,nicknameNoFirstIndex)=>firstLetter.concat(nicknameNoFirstIndex)
+        // // // const readyNickArr=readyNickname(firstLetter,nicknameNoFirstIndex)
+        // // // const nick=readyNickArr.join("")
+        // // // const mappedNameWithNick=people.map(people=>{
+        // // //     const firstName=people.firstName
+        // // //     const lastName=people.lastName
+        // // //     const id={
+        // // //         firstName:firstName,
+        // // //         lastName:lastName,
+        // // //         nick:nick
+               
+        // // //     }
+        // // //     return id
+        // // // })
+        
+        // // // // console.log(mappedName)
+        
+        
+        
+        
+        
+        // // // console.log(readyNickname(firstLetterUppercase,nicknameNoFirstIndex))
+        
+        
+        // // //     console.log(firstLetterToUppercase())
+        // // // const changeFirstLetter=(firstLetterToUppercase,nicknameArr)=>{
+        // // //     nicknameArr
+        // // // 
