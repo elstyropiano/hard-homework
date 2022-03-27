9fc3c338e0684c32a43c4e861426edee9d4301b3
Grzegorz W <gerwazy911@gmail.com>
2022-03-27 01:48:46 +0100

    update

--- task1.js
+++ task1.js
@@ -15,159 +15,219 @@ const people = [
 
 /////////////////////////////////////////////////////////////
 
-function createNickname(people){                                                 // function CreateNickname add new key in Object like nickname 
-  const getName=people.map(firstName=>firstName.firstName)
-  const Name= getName=>{ 
-    for(i=0;i<getName.length;i++)                        
-    return getName[i] 
-    return name
-  }   
-  const name=Name(getName)  
-  const getLastName=people.map(lastName=>lastName.lastName)                    // ready function return firstName and lastNAME from Obj
-  const LastName= getLastName=>{ 
-    for(i=0;i<getLastName.length;i++)                      
-      return getLastName[i]
-      return LastName(getLastName)                                                /// wtorek 17-2   //sroda 17-1 //Czwartek 17.30-23// 
-  }
-  const lastName=LastName(getLastName) 
+// function createNickname(people){                                                 // function CreateNickname add new key in Object like nickname 
+//   const getName=people.map(firstName=>firstName.firstName)
+//   const Name= getName=>{ 
+//     for(i=0;i<getName.length;i++)                        
+//     return getName[i] 
+// //     return name
+// //   }  
+// const CreateNickname=people=>people.map(get)
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //  Write function which check what arguments was pass to function (name or last name) return name or reverse lastName/// remeber to use only one line in function{}   //
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  const nameOrLastName=(name,lastName)=>nameOrLastName=name?  name: lastName.reverse()
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //     Write function which take a function as argument with Name or LastName parameters and return 3 fist letters from argument              //
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-const ThreeLetters=nameOrLastName=>{
-  const lettersArr=[]
-  for(i=0;i<3;i++)
-  return lettersArr.push(nameOrLastName[i])
-}
-  
-
-
-
-console.log(createNickname(people))
-
-
-
-
-
-
-
-
-
-
-
-
-  /////////////////////////////////////////////////////////////////
- 
-
-    //  if(nameOrLastName.length>3){
-    //    
-    //     }} 
-    //     return lettersArr.reverse()
-      
-    
-
-  ////////////////////////////////////////////////////////////
-     
-   
-
+const createNickname=people=>{
+  people.reduce().map((getNames ,getLastNames)=>{
+    getNames=people.map(firstName=>firstName.firstName) 
+    const getNamesLoop= getName=>{
+      for(i=0;i<getName.length;i++) return getName[i]
+    const name=getNamesLoop(getNames)
+    getLastNames=people.map(lastName=>lastName.lastName)              ////sprawdzic czy ten kod jest potrzebny
+    const getLastNamesLoop= getLastName=>{ 
+      for(i=0;i<getLastName.length;i++) return getLastName[i]         //stworzyc funkcje ktora bedzie miala petle for oin ktora zwroci mi indexy tablicy                                       /// wtorek 17-2   //sroda 17-1 //Czwartek 17.30-23// 
+    }
+    const lastName=getLastNamesLoop(getLastNames).split('').reverse()  
+    getThreeLetters=(nameOrLastName,getNames)=>{
+      lettersArr=[]
+      for(i=0;i<3;i++){ 
+      lettersArr.push(name[i])}                                                                            //check with unshift
+      const reversedOrNoReversed=getNames.indexOf(nameOrLastName)? lettersArr:lettersArr.reverse()                 // 6 h on this function 
+      return reversedOrNoReversed
+      }
+      const threeLettersName=getThreeLetters(name,getNames)
+      const threeLettersLastName=getThreeLetters(lastName,getNames)
+    // function getNickname(reduce((threeLettersName,threeLettersLastName=>{
+    //       threeLettersName+threeLettersLastName
+    // }))))
 
   
+    // (threeLettersName,threeLettersLastName)=>threeLettersName.concatthreeLettersLastName)
+    // return getNickname(threeLettersName,threeLettersLastName)
  
- 
-   
-   
-     
-    //  return getThreeReverseNamesLetters(getNames)
-  
-
+      console.log(createNickname(people))
+                                              
 
  
 
-
-
-
-
-
-      // const getLastName=people.map(getLastName=>getLastName.getLastName)      
-        // const names=people.map(firstName=>firstName.firstName)                   // return simple name
-                                         // return simple name
-// const LastName=people.map(getLastName=>getLastName.getLastName) 
-
-//   function  getName()){                              
-// for(i=0;i<getNames.length;i++)                            
-// return getNames[i]                                                // retrun simple last name
-
-                                                      //retrun simple last name
-
-// console.log(createNickname(people))
-//   const getThreeReverseNamesLetters=getName=>{  
-//    const lettersArr=[]
-//     if(getName.length>3){
-//       for(i=0;i<3;i++){
-//         let letter=getNames[i]
-//        return lettersArr.push(letter)
-        
-//       }}}
-//       return getThreeReverseNamesLetters(getName(getNames))
-//     }
  
+  ///map i dac 8 zmiennych albo wszytkie 
 
 
-  
-  
-//     }}
-//     return getThreeReverseNamesLetters(getNames)
-//  }
 
-//  console.log(createNickname(people))
- 
-
-  
-
-
-  
-    
-  //  const lettersArr=[]
-  //  const reversed=lettersArr.push(lastNames)
-  //  return reversed
-  
-
- 
-// const slowa={
-    
-//     lastName: "Wjkasdkjas",
+//  function createNickname(people){  
+//   const getNames=people.map(firstName=>firstName.firstName) 
+//   const getNamesLoop= getName=>{
+//     for(i=0;i<getName.length;i++) return getName[i]
+//   }                                                                    // function CreateNickname add new key in Object like nickname                                                                                                       // function CreateNickname add new key in Object like nickname 
+//   const name=getNamesLoop(getNames)
+//   const getLastNames=people.map(lastName=>lastName.lastName)////sprawdzic czy ten kod jest potrzebny
+//   const getLastNamesLoop= getLastName=>{ 
+//     for(i=0;i<getLastName.length;i++) return getLastName[i]         //stworzyc funkcje ktora bedzie miala petle for oin ktora zwroci mi indexy tablicy                                       /// wtorek 17-2   //sroda 17-1 //Czwartek 17.30-23// 
 //   }
+//   const lastName=getLastNamesLoop(getLastNames).split('').reverse()  
+//   // const getObj=people.map(people=>people)
+ 
   
-// console.log(getThreeReverseNamesLetters(slowa))
+//   // .split('').reverse() 
+
+//   // const name=Name(getName)  
+//   // const getLastName=people.map(lastName=>lastName.lastName)                    // ready function return firstName and lastNAME from Obj
+//   // const LastName= getLastName=>{ 
+//   //   for(i=0;i<getLastName.length;i++)                      
+//   //     return getLastName[i]
+//   //     return LastName(getLastName)                                                /// wtorek 17-2   //sroda 17-1 //Czwartek 17.30-23// sobota od 17:00
+//   // }
+  
+//   // const lastName=LastName(getLastName) 
+
+//   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//   //  Write function which check what arguments was pass to function (name or last name) return name or reverse lastName/// remeber to use only one line in function{}   //
+//   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//   // const nameOrLastName=(name,lastName)=>name?  name: lastName.reverse()
+
+//   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//   //     Write function which take a function as argument with Name or LastName parameters and return 3 fist letters from argument              //
+//   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
-// const getThreeLastReverseLetaters=arr.map(lastName=>{
-//     const reverse=lastName.reverse()
-//     const lettersArr=[]
-//     if(lastName.length>3){
-//         for(i=0;i<3;i++){
-//             let letter=getName[i]
-//             lettersArr.push(letter)  
-//         }
-//         return lettersArr.reverse()
+  
+//   getThreeLetters=(nameOrLastName,getNames)=>{
+//     lettersArr=[]
+//     for(i=0;i<3;i++){ 
+//     lettersArr.push(name[i])}                                                                            //check with unshift
+//     const reversedOrNoReversed=getNames.indexOf(nameOrLastName)? lettersArr:lettersArr.reverse()                 // 6 h on this function 
+//     return reversedOrNoReversed
 //     }
-//     else 
-//     for(i=0;i<getName.length;i++){
-//         let letter=getName[i]
-//         lettersArr.push(letter) 
-//      }
+//                                                                                                             //zwrocic parametr z poprzedniej funcki
+   
+//       const threeLettersName=getThreeLetters(name,getNames)
+//       const threeLettersLastName=getThreeLetters(lastName,getNames)
 
-//      return lettersArr.reverse()
-//     })
-//     for(i=0;i<3;i++){
+//   // getNickname=(threeLettersName,threeLettersLastName)=>{
+//   //    const nickname= threeLettersName.concat(threeLettersLastName)
+//   //    return nickname
+//   //   }
+//   //   return getNickname(threeLettersName,threeLettersLastName)
+//   // const threeLettersName=getThreeLetters(name,getNames)
+//   // }
+//   getNickname=(threeLettersName,threeLettersLastName)=>threeLettersName.concat(threeLettersLastName)
+  
+//   return threeLettersLastName
+
+//  }
+//     console.log(createNickname(people))
+
+
+//     // const reversedOrNoReversed=getNames.indexOf(name)? lettersArr.reverse():lettersArr.reverse()          //always true ///fucking bug i spent 2h on him
+
+//       ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//   //     Write function which take a function as argument with Name or LastName parameters and return 3 fist letters from argument              //
+//   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// // const threeLetters=nameOrLastName=>{
+// //   const lettersArr=[]
+// //   for(i=0;i<3;i++){
+// //     lettersArr.push(nameOrLastName[i])
+// //   }
+// //   return lettersArr
+// // }
+
+
+// // return threeLetters(name)
+
+// // }
+
+
+// // console.log(createNickname(people))
+//   /////////////////////////////////////////////////////////////////
+ 
+
+//     //  if(nameOrLastName.length>3){
+//     //    
+//     //     }} 
+//     //     return lettersArr.reverse()
+//   ////////////////////////////////////////////////////////////
+
+//     //  return getThreeReverseNamesLetters(getNames)
+  
+
+
+//       // const getLastName=people.map(getLastName=>getLastName.getLastName)      
+//         // const names=people.map(firstName=>firstName.firstName)                   // return simple name
+//                                          // return simple name
+// // const LastName=people.map(getLastName=>getLastName.getLastName) 
+
+// //   function  getName()){                              
+// // for(i=0;i<getNames.length;i++)                            
+// // return getNames[i]                                                // retrun simple last name
+
+//                                                       //retrun simple last name
+
+// // console.log(createNickname(people))
+// //   const getThreeReverseNamesLetters=getName=>{  
+// //    const lettersArr=[]
+// //     if(getName.length>3){
+// //       for(i=0;i<3;i++){
+// //         let letter=getNames[i]
+// //        return lettersArr.push(letter)
+        
+// //       }}}
+// //       return getThreeReverseNamesLetters(getName(getNames))
+// //     }
+ 
+
+
+  
+  
+// //     }}
+// //     return getThreeReverseNamesLetters(getNames)
+// //  }
+
+// //  console.log(createNickname(people))
+ 
+//   //  const lettersArr=[]
+//   //  const reversed=lettersArr.push(lastNames)
+//   //  return reversed
+  
+
+ 
+// // const slowa={
+    
+// //     lastName: "Wjkasdkjas",
+// //   }
+  
+// // console.log(getThreeReverseNamesLetters(slowa))
+
+
+
+// // const getThreeLastReverseLetaters=arr.map(lastName=>{
+// //     const reverse=lastName.reverse()
+// //     const lettersArr=[]
+// //     if(lastName.length>3){
+// //         for(i=0;i<3;i++){
+// //             let letter=getName[i]
+// //             lettersArr.push(letter)  
+// //         }
+// //         return lettersArr.reverse()
+// //     }
+// //     else 
+// //     for(i=0;i<getName.length;i++){
+// //         let letter=getName[i]
+// //         lettersArr.push(letter) 
+// //      }
+
+// //      return lettersArr.reverse()
+// //     })
+// //     for(i=0;i<3;i++){
 //         let letter=reverse[i]
 //         threeLetters.push(letter)
 //     }
